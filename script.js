a9ae1af2d562626e3e5fd8cae622a6f1d814ed34
Nuriddin <nuriddinyu@synel.co.il>
2022-02-14 11:26:13 +0500

    lesson 1 finished

---
+++ script.js
@@ -0,0 +1,37 @@
+let bekhruz = 13;
+
+//  if   -  если
+// Сравнения: ==,  ===, >, <, >=, <=
+
+// СРАВНЕНИЕ ВСЕГДА ВОЗРАЩАЕТ ЛИБО TRUE ЛИБО FALSE
+
+console.log(bekhruz == 12);
+console.log(bekhruz === 12);
+
+console.log(bekhruz == '12');
+console.log(bekhruz === '12');
+
+console.log('Больше', bekhruz > 11);
+console.log('Меньше', bekhruz < 15);
+
+console.log('Больше либо равно', bekhruz <= 12);
+console.log('Меньше либо равно', bekhruz <= 12);
+
+// console.log('', bekhruz = 12); // Не СРАВНЕНИЕ
+
+
+if (bekhruz > 12) {
+    console.log('Бехрузу больше 12 лет')
+}
+if (bekhruz < 12) {
+    let b = 12;
+    console.log('Бехрузу меньше 12 лет')
+}
+
+// && - and И
+// || - or ИЛИ
+
+console.log('Использование И', 12 === 12 && 12 === 13);
+console.log('Использование ИЛИ', 12 === 12 || 12 === 13);
+
+// НУЖНО ВЫВЕСТИ БЕХРУЗ БОЛЬШЕ 18 ЛЕТ , РАВНЯЕТСЯ 18 лет, МЕНЬШЕ 18 лет
